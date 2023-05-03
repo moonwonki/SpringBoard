4e70cc5f8cf827d717c3f3ec12ebd6ef841aba48
moonwonki <moonwonki1999@gmail.com>
2023-05-03 16:57:20 +0900

    feat: first commit

---
+++ src/main/java/boardStudy/boardStudy/BoardStudyApplication.java
@@ -0,0 +1,13 @@
+package boardStudy.boardStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/boardStudy/boardStudy/BoardStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package boardStudy.boardStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
