03ad8a0c5f8c18e36a9ae76fa3ce0c8902ef6897
moonwonki <moonwonki1999@gmail.com>
2023-05-21 16:21:26 +0900

    feat. BoardService 분리

--- src/main/java/boardStudy/boardStudy/controller/BoardController.java
+++ src/main/java/boardStudy/boardStudy/controller/BoardController.java
@@ -1,7 +1,7 @@
 package boardStudy.boardStudy.controller;
 
 import boardStudy.boardStudy.domain.Board;
-import boardStudy.boardStudy.repository.BoardRepository;
+import boardStudy.boardStudy.service.BoardService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,8 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
-import java.util.Collections;
-import java.util.List;
+
 
 
 @Controller
@@ -19,27 +18,23 @@ import java.util.List;
 @RequestMapping
 public class BoardController {
 
-    private final BoardRepository boardRepository;
+    private final BoardService boardService;
 
     @Autowired
-    public BoardController(BoardRepository boardRepository) {
-        this.boardRepository = boardRepository;
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
     }
 
     @GetMapping("/boards")
     public String getBoardList(Model model){
-        log.info("메인 메뉴 불러옴");
-        List<Board> boards = boardRepository.findAll();
-        Collections.reverse(boards);
-
-        model.addAttribute("boards", boards);
+        model.addAttribute("boards", boardService.getBoardList());
         return "html/boards";
     }
 
     @GetMapping("/board/{id}")
     public String getBoardById(Model model, @PathVariable("id") Long id){
-        Board board = boardRepository.findById(id).get();
-        model.addAttribute("board", board);
+
+        model.addAttribute("board", boardService.findById(id));
         return "html/board";
     }
 
@@ -52,28 +47,24 @@ public class BoardController {
     @PostMapping("/create")
     public String createBoard(@ModelAttribute Board board, RedirectAttributes redirectAttr){
 
-        Board savedBoard = boardRepository.save(board);
+        Long savedBoardId = boardService.save(board);
 
-        redirectAttr.addAttribute("id", savedBoard.getId());
-        log.info("신규 보드 생성: " + savedBoard.getId());
+        redirectAttr.addAttribute("id", savedBoardId);
+        log.info("신규 보드 생성: " + savedBoardId);
         return "redirect:/board/{id}";
     }
 
     @GetMapping("/board/recommend/{id}")
     public String recommendBoard(@PathVariable("id") Long boardId, RedirectAttributes redirectAttr) {
-        Board board = boardRepository.findById(boardId).orElseThrow(() -> new IllegalArgumentException("Invalid board id: " + boardId));
-        int recommendCount = board.getRecommendCount();
-
-        board.setRecommendCount(recommendCount + 1);
-        boardRepository.save(board);
-        redirectAttr.addAttribute("id", board.getId());
+        boardService.recommendBoard(boardId);
+        redirectAttr.addAttribute("id", boardId);
         return "redirect:/board/{id}";
     }
 
     @DeleteMapping("/board/{id}")
     public String deleteBoard(Model model, @PathVariable("id") Long id, RedirectAttributes redirectAttr){
         log.info("게시글 삭제: " + id);
-        boardRepository.deleteById(id);
+        boardService.deleteById(id);
 
 
         return "redirect:/boards";
@@ -81,18 +72,14 @@ public class BoardController {
 
     @GetMapping("/board/update/{id}")
     public String getEditForm(Model model, @PathVariable("id") Long id){
-        Board board = boardRepository.findById(id).get();
-        model.addAttribute("board", board);
+
+        model.addAttribute("board", boardService.findById(id));
         return "html/boardEditForm";
     }
 
     @PostMapping("/board/update/{id}")
     public String editBoard(Model model, @ModelAttribute Board newBoard, @PathVariable("id") Long id){
-        Board oldBoard = boardRepository.findById(id).get();
-        oldBoard.setTitle(newBoard.getTitle());
-        oldBoard.setAuthor(newBoard.getAuthor());
-        oldBoard.setContent(newBoard.getContent());
-        boardRepository.save(oldBoard);
+        boardService.updateById(id, newBoard.getTitle(), newBoard.getAuthor(), newBoard.getContent());
         return "redirect:/board/" + id;
     }
 }
